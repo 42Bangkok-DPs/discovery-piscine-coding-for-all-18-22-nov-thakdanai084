e368a90f5525a1d88bd8ca9c6e58ff84a6277a1a
thakdanai084 <67030084@kmitl.ac.th>
2024-11-21 06:44:08 +0000

    password

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+entered_password = input("Please enter the password: ")
+if entered_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
